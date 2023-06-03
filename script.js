2e04128a200db510b2011c33c5f4612e4443fa8a
Full Name <njdowdle@gmail.com>
2023-06-04 03:28:04 +0700

    added everything except decimal

--- script.js
+++ script.js
@@ -25,77 +25,217 @@ const buttonDelete = document.querySelector('#delete');
 const buttonDecimal = document.querySelector('#buttonDecimal');
 const buttonEquals = document.querySelector('#buttonEquals');
 
+let firstNumber;
+let operater;
+let secondNumber = "";
+let readyForSecondNumber = false;
 let globalText = "";
+
+const operate = function(operation, number1, number2) {
+    readyForSecondNumber = false;
+    secondNumber = "";
+    operater = null;
+    if(operation === "+") {
+        globalText = add(parseInt(number1), parseInt(number2));
+    } else if (operation === "-") {
+        globalText = minus(parseInt(number1), parseInt(number2));
+    } else if (operation === "X") {
+        globalText = multiply(parseInt(number1), parseInt(number2));
+    } else if (operation === "/") {
+        globalText = divide(parseInt(number1), parseInt(number2));
+    } else {
+        console.log("Error");
+    }
+    return globalText;
+}
+
+const add = function(number1, number2) {
+    return number1 + number2;
+}
+
+const minus = function(number1, number2) {
+    return number1 - number2;
+}
+
+const multiply = function(number1, number2) {
+    return number1 * number2;
+}
+
+const divide = function(number1, number2) {
+    return number1 / number2;
+}
 
 button0.addEventListener('click', () => {
     globalText = globalText + "0";
     display.innerHTML = globalText;
+    if(readyForSecondNumber) {
+        secondNumber = secondNumber + "0";
+    }
 });
 
 button1.addEventListener('click', () => {
     globalText = globalText + "1";
     display.innerHTML = globalText;
+    if(readyForSecondNumber) {
+        secondNumber = secondNumber + "1";
+    }
 });
 
 button2.addEventListener('click', () => {
     globalText = globalText + "2";
     display.innerHTML = globalText;
+    if(readyForSecondNumber) {
+        secondNumber = secondNumber + "2";
+    }
 });
 
 button3.addEventListener('click', () => {
     globalText = globalText + "3";
     display.innerHTML = globalText;
-    console.log(globalText);
+    if(readyForSecondNumber) {
+        secondNumber = secondNumber + "3";
+    }
 });
 
 button4.addEventListener('click', () => {
     globalText = globalText + "4";
     display.innerHTML = globalText;
+    if(readyForSecondNumber) {
+        secondNumber = secondNumber + "4";
+    }
 });
 
 button5.addEventListener('click', () => {
     globalText = globalText + "5";
     display.innerHTML = globalText;
+    if(readyForSecondNumber) {
+        secondNumber = secondNumber + "5";
+    }
 });
 
 button6.addEventListener('click', () => {
     globalText = globalText + "6";
     display.innerHTML = globalText;
+    if(readyForSecondNumber) {
+        secondNumber = secondNumber + "6";
+    }
 });
 
 button7.addEventListener('click', () => {
     globalText = globalText + "7";
     display.innerHTML = globalText;
+    if(readyForSecondNumber) {
+        secondNumber = secondNumber + "7";
+    }
 });
 
 button8.addEventListener('click', () => {
     globalText = globalText + "8";
     display.innerHTML = globalText;
+    if(readyForSecondNumber) {
+        secondNumber = secondNumber + "8";
+    }
 });
 
 button9.addEventListener('click', () => {
     globalText = globalText + "9";
     display.innerHTML = globalText;
+    if(readyForSecondNumber) {
+        secondNumber = secondNumber + "9";
+    }
 });
 
 buttonAdd.addEventListener('click', () => {
-    globalText = globalText + " + ";
-    display.innerHTML = globalText;
+    // need to make sure only use 1 divide, multiply, minus, or add
+    if(operater === undefined || operater === null) {
+        // store the first number
+        firstNumber = globalText;
+        operater = "+";
+        readyForSecondNumber = true;
+
+        globalText = globalText + " + ";
+        display.innerHTML = globalText;
+    } else if (operater.length === 1) {
+        if(secondNumber != null) {
+            globalText = operate(operater, firstNumber, secondNumber);
+            firstNumber = globalText;
+            operater = "+";
+            readyForSecondNumber = true;
+            globalText = globalText + " + ";
+            display.innerHTML = globalText;
+        } else {
+            console.log("ERROR!!");
+        }
+    }
 });
 
 buttonMinus.addEventListener('click', () => {
-    globalText = globalText + " - ";
-    display.innerHTML = globalText;
+    // need to make sure only use 1 divide, multiply, minus, or add
+    if(operater === undefined || operater === null) {
+        firstNumber = globalText;
+        operater = "-";
+        readyForSecondNumber = true;
+
+        globalText = globalText + " - ";
+        display.innerHTML = globalText;
+    } else if (operater.length === 1) {
+        if(secondNumber != null) {
+            globalText = operate(operater, firstNumber, secondNumber);
+            firstNumber = globalText;
+            operater = "-";
+            readyForSecondNumber = true;
+            globalText = globalText + " - ";
+            display.innerHTML = globalText;
+        } else {
+            console.log("ERROR!");
+        }
+    }
 });
 
 buttonMultiply.addEventListener('click', () => {
-    globalText = globalText + " * ";
-    display.innerHTML = globalText;
+    // need to make sure only use 1 divide, multiply, minus, or add
+    if(operater === undefined || operater === null) {
+        // store the first number
+        firstNumber = globalText;
+        operater = "X";
+        readyForSecondNumber = true;
+
+        globalText = globalText + " * ";
+        display.innerHTML = globalText;
+    } else if (operater.length === 1) {
+        if(secondNumber != null) {
+            globalText = operate(operater, firstNumber, secondNumber);
+            firstNumber = globalText;
+            operater = "X";
+            readyForSecondNumber = true;
+            globalText = globalText + " * ";
+            display.innerHTML = globalText;
+        } else {
+            console.log("Error!");
+        }
+    }
 });
 
 buttonDivide.addEventListener('click', () => {
-    globalText = globalText + " / ";
-    display.innerHTML = globalText;
+    // need to make sure only use 1 divide, multiply, minus, or add
+    if(operater === undefined || operater === null) {
+        // store the first number
+        firstNumber = globalText;
+        operater = "/";
+        readyForSecondNumber = true;
+
+        globalText = globalText + " / ";
+        display.innerHTML = globalText;
+    } else if (operater.length === 1) {
+        if(secondNumber != null) {
+            globalText = operate(operater, firstNumber, secondNumber);
+            firstNumber = globalText;
+            operater = "/";
+            readyForSecondNumber = true;
+            globalText = globalText + " / ";
+            display.innerHTML = globalText;
+        }
+    }
 });
 
 buttonDecimal.addEventListener('click', () => {
@@ -106,15 +246,29 @@ buttonDecimal.addEventListener('click', () => {
 });
 
 buttonEquals.addEventListener('click', () => {
-
+    //get second number value
+    if(secondNumber != null && secondNumber != undefined) {
+        globalText = operate(operater, firstNumber, secondNumber);
+        firstNumber = globalText;
+        display.innerHTML = globalText;
+    } else {
+        console.log("ERROR!!!");
+    }
 });
 
 buttonClear.addEventListener('click', () => {
+    firstNumber = "";
+    secondNumber = "";
+    operater = null;
+    readyForSecondNumber = false;
     globalText = "";
     display.innerHTML = globalText;
 });
 
 buttonDelete.addEventListener('click', () => {
+    // need to make sure if remove * / - + then ready for second number is deleted
+    // need to make delete also delete the correct number first or second number
+
     globalText = globalText.slice(0, globalText.length - 1);
     display.innerHTML = globalText;
 });
